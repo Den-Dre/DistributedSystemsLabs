ef44f491fd3fa3fb14bb2b358d26801974e29b53
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2021-12-01 18:31:51 +0100

    Add GET request to retrieve public keys

--- GoogleCloud/src/main/java/be/kuleuven/distributedsystems/cloud/auth/SecurityFilter.java
+++ GoogleCloud/src/main/java/be/kuleuven/distributedsystems/cloud/auth/SecurityFilter.java
@@ -1,9 +1,13 @@
 package be.kuleuven.distributedsystems.cloud.auth;
 
+import be.kuleuven.distributedsystems.cloud.entities.Seat;
 import be.kuleuven.distributedsystems.cloud.entities.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -11,17 +15,27 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.*;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private final WebClient.Builder builder = WebClient.builder();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -29,7 +43,10 @@ public class SecurityFilter extends OncePerRequestFilter {
         User user;
         if (session != null) {
             try {
-                DecodedJWT jwt = JWT.decode(session.getValue());
+                getKeys();
+                DecodedJWT jwt= JWT.decode(session.getValue());
+                var kid = jwt.getKeyId();
+
                 String role = jwt.getClaim("role").asString();
                 if (!"manager".equals(role))
                         role = "";
@@ -52,6 +69,41 @@ public class SecurityFilter extends OncePerRequestFilter {
             context.setAuthentication(new FirebaseAuthentication(user));
         }
         filterChain.doFilter(request, response);
+    }
+
+    private void getKeys() {
+        URL url;
+        HttpURLConnection con = null;
+        try {
+            url = new URL("https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            int status = con.getResponseCode();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            System.out.println("GET request contents: " + content);
+            System.out.println("GET request status: " +  status);
+            in.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (con != null)
+                con.disconnect();
+        }
+
+//        return builder
+//                .baseUrl("https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+//                .build()
+//                .get()
+//                .retrieve()
+//                .bodyToMono(new ParameterizedTypeReference<CollectionModel<String>>() {})
+//                .block()
+//                .getContent();
     }
 
     @Override
